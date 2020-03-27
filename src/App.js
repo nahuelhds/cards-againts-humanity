fc7f9e181677175712ea8f65cb8538f8d4743462
Nahuel Sotelo <nahuel.sotelo@mercadolibre.com>
2020-03-26 22:06:12 -0300

    Multiplayer with websocket

--- src/App.js
+++ src/App.js
@@ -1,113 +1,40 @@
 // src/App.js
 
-import { Client } from "boardgame.io/react";
 import React from "react";
+import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
+import { TicTacToe } from "./game";
+import { TicTacToeBoard } from "./board";
 
-class TicTacToeBoard extends React.Component {
-  onClick(id) {
-    if (this.isActive(id)) {
-      this.props.moves.clickCell(id);
-      this.props.events.endTurn();
-    }
-  }
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: "localhost:8000" })
+});
 
-  isActive(id) {
-    if (!this.props.isActive) return false;
-    if (this.props.G.cells[id] !== null) return false;
-    return true;
-  }
+class App extends React.Component {
+  state = { playerID: null };
 
   render() {
-    let winner = "";
-    if (this.props.ctx.gameover) {
-      winner =
-        this.props.ctx.gameover.winner !== undefined ? (
-          <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
-        ) : (
-          <div id="winner">Draw!</div>
-        );
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      );
     }
-
-    const cellStyle = {
-      border: "1px solid #555",
-      width: "50px",
-      height: "50px",
-      lineHeight: "50px",
-      textAlign: "center"
-    };
-
-    let tbody = [];
-    for (let i = 0; i < 3; i++) {
-      let cells = [];
-      for (let j = 0; j < 3; j++) {
-        const id = 3 * i + j;
-        cells.push(
-          <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
-            {this.props.G.cells[id]}
-          </td>
-        );
-      }
-      tbody.push(<tr key={i}>{cells}</tr>);
-    }
-
     return (
       <div>
-        <table id="board">
-          <tbody>{tbody}</tbody>
-        </table>
-        {winner}
+        <TicTacToeClient playerID={this.state.playerID} />
       </div>
     );
   }
 }
-
-// Return true if `cells` is in a winning configuration.
-function IsVictory(cells) {
-  return false;
-}
-
-// Return true if all `cells` are occupied.
-function IsDraw(cells) {
-  return cells.filter(c => c === null).length == 0;
-}
-
-const TicTacToe = {
-  setup: () => ({ cells: Array(9).fill(null) }),
-
-  moves: {
-    clickCell: (G, ctx, id) => {
-      if (G.cells[id] === null) {
-        G.cells[id] = ctx.currentPlayer;
-      }
-    }
-  },
-
-  turn: {
-    moveLimit: 1
-  },
-
-  ai: {
-    enumerate: (G, ctx) => {
-      let moves = [];
-      for (let i = 0; i < 9; i++) {
-        if (G.cells[i] === null) {
-          moves.push({ move: "clickCell", args: [i] });
-        }
-      }
-      return moves;
-    }
-  },
-
-  endIf: (G, ctx) => {
-    if (IsVictory(G.cells)) {
-      return { winner: ctx.currentPlayer };
-    }
-    if (IsDraw(G.cells)) {
-      return { draw: true };
-    }
-  }
-};
-
-const App = Client({ game: TicTacToe, board: TicTacToeBoard });
 
 export default App;
