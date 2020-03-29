32e506ad7cf61e3fd794db5923b3309d43b34305
Nahuel Sotelo <nahuel.sotelo@mercadolibre.com>
2020-03-29 03:20:10 -0300

    White card replacement when there is no {whiteCard}

---
+++ src/CardsAgainstHumanity/constants.js
@@ -0,0 +1,4 @@
+export const MAX_WHITE_CARDS = 10;
+export const STAGE_DRAW_BLACK_CARD = "drawBlackCard";
+export const STAGE_SELECT_WHITE_CARDS = "selectWhiteCards";
+export const STAGE_CHOOSE_WINNER = "chooseWinner";
