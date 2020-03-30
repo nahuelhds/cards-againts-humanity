65b33ec5c7475349ad2f55e9267dc59e0e2ac537
Nahuel Sotelo <nahuel.sotelo@mercadolibre.com>
2020-03-30 09:32:05 -0300

    Player selection font size

--- src/CardsAgainstHumanity/components/PlayerSelection/PlayerSelectionContainer.js
+++ src/CardsAgainstHumanity/components/PlayerSelection/PlayerSelectionContainer.js
@@ -7,21 +7,21 @@ export default class PlayerSelectionContainer extends Component {
       <div className="flex flex-row h-screen bg-gray-200">
         <Link
           to="/room/0/player/0"
-          className="flex-1 flex justify-center items-center text-gray-700 bg-gray-400 m-4"
+          className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
           Jugador 0
         </Link>
         <Link
           to="/room/0/player/1"
-          className="flex-1 flex justify-center items-center text-gray-700 bg-gray-400 m-4"
+          className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
           Jugador 1
         </Link>
         <Link
           to="/room/0/player/2"
-          className="flex-1 flex justify-center items-center text-gray-700 bg-gray-400 m-4"
+          className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
-          Jugaodr 2
+          Jugador 2
         </Link>
       </div>
     );
