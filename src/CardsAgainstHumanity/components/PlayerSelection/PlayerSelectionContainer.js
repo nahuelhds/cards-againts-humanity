b1b8b2ea56fe45990507dea808ed47f6ee751483
Nahuel Sotelo <nahuel.sotelo@mercadolibre.com>
2020-03-30 21:50:11 -0300

    Typo

--- src/CardsAgainstHumanity/components/PlayerSelection/PlayerSelectionContainer.js
+++ src/CardsAgainstHumanity/components/PlayerSelection/PlayerSelectionContainer.js
@@ -6,25 +6,25 @@ export default class PlayerSelectionContainer extends Component {
     return (
       <div className="flex flex-row h-screen bg-gray-200">
         <Link
-          to="/room/default/size/4/player/0"
+          to="/game/default/size/4/player/0"
           className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
           Jugador 0
         </Link>
         <Link
-          to="/room/default/size/4/player/1"
+          to="/game/default/size/4/player/1"
           className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
           Jugador 1
         </Link>
         <Link
-          to="/room/default/size/4/player/2"
+          to="/game/default/size/4/player/2"
           className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
           Jugador 2
         </Link>
         <Link
-          to="/room/default/size/4/player/3"
+          to="/game/default/size/4/player/3"
           className="flex-1 flex justify-center items-center text-3xl text-gray-700 bg-gray-400 m-4"
         >
           Jugador 3
